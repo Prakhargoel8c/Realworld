87997c7709d324983d3bca1aabc38a7507b8c6a9
Prakhar <Prakhar@redbirdvr.com>
2019-08-30 17:45:49 +0530

    Player movement and interactalble object highlight

---
+++ Assets/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    public KeyCode forward;
+    public KeyCode backward;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode interactionKey;
+    private Vector2 playerDirection;
+    private bool playerTryingToInteract;
+    private Rigidbody2D rigidbody;
+    private Vector3 oldposition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oldposition = (transform.position);
+        playerDirection = Vector2.zero;
+        if (Input.GetKey(left)) playerDirection.x -= 1;
+        if (Input.GetKey(right)) playerDirection.x += 1;
+        if (Input.GetKey(forward)) playerDirection.y += 1;
+        if (Input.GetKey(backward)) playerDirection.y -= 1;
+        if (Input.GetKey(interactionKey)) playerTryingToInteract = true;
+        rigidbody.velocity = playerDirection*GameConstants.MovementOffset;
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+    }
+}
---
+++ Assets/PlayerInteractions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    public Color color;
+    public float interactionDistance = 3f;
+    private Collider2D[] collider2D;
+    private GameObject currentObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentObject = null;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, interactionDistance);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        LayerMask mask = LayerMask.GetMask("Interactable");
+        collider2D = Physics2D.OverlapCircleAll(transform.position, interactionDistance,mask);
+        if(currentObject!=collider2D[0])
+        {
+            if(currentObject!=null)
+            {
+                currentObject.GetComponent<Outline>().effectDistance = Vector2.zero;
+            }
+            currentObject = collider2D[0].gameObject;
+            currentObject.GetComponent<Outline>().effectColor = color;
+            currentObject.GetComponent<Outline>().effectDistance = new Vector2(7, 7);
+        }
+        Debug.Log(collider2D[0]);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(currentObject!=null)
+        {
+            currentObject.GetComponent<Outline>().effectDistance = Vector2.zero;
+        }
+    }
+}
